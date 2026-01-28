985f9efc9d048f3673f4a3cab6fa8583f09d9db4
Lucas Huang <lucas.huang@menloschool.org>
2026-01-27 22:25:57 -0800

    Finished V1

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 /**
  * The class Making Change solves a classic problem:
  * given a set of coins, how many ways can you make change for a target amount?
  *
  * @author Zach Blick
- * @author [YOUR NAME HERE]
+ * @author Lucas
  */
 
 public class MakingChange {
@@ -12,6 +14,24 @@ public class MakingChange {
      *  for any given total with any given set of coins.
      */
     public static long countWays(int target, int[] coins) {
+
+        ArrayList<Integer> solutions = new ArrayList<Integer>();
         return 0;
+    }
+
+    public static ArrayList<Integer> findSolution(int target, int[] coins, ArrayList<Integer> path, int count) {
+        if (count == target) {
+            return path;
+        }
+        if (count > target) {
+            return null;
+        }
+        for (int i = 0; i < coins.length; i++) {
+            ArrayList<Integer> tempPath = path;
+            tempPath.add(coins[i]);
+            findSolution(target, coins, tempPath, count + coins[i]);
+        }
+
+        return path;
     }
 }
